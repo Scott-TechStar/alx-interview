e93487e15019bd3c8c72b0faefb19e00c145f54e
John Mwadime <johnniekips@gmail.com>
2024-04-10 12:34:36 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,43 +1,63 @@
 #!/usr/bin/python3
-'''
-Module that solves the N Queens puzzle
-'''
-from sys import argv, exit
+"""
+    Solve the N Queens Problem
+    (See README for instruction)
+"""
+
+from sys import argv
+
+
+def nQueens(n: int) -> None:
+    """
+        Finds all possible ways to place n queens on an
+        n x n board without them attacking each other
+    """
+    board = []
+    usedCols = set()
+    nDiagonals = set()
+    pDiagonals = set()
+
+    def backtrack(row: int) -> None:
+        """Prints all possible solution"""
+        if row == n:
+            print(board)
+            return
+
+        for col in range(n):
+            if col in usedCols or col - row in nDiagonals\
+                    or col + row in pDiagonals:
+                continue
+            usedCols.add(col)
+            nDiagonals.add(col - row)
+            pDiagonals.add(col + row)
+            board.append([row, col])
+
+            backtrack(row + 1)
+
+            usedCols.remove(col)
+            nDiagonals.remove(col - row)
+            pDiagonals.remove(col + row)
+            board.remove([row, col])
+
+    backtrack(0)
+
+    # for row in board:
+    #     print(row)
+
 
 if __name__ == "__main__":
     if len(argv) != 2:
-        print('Usage: nqueens N')
+        print("Usage: nqueens N")
         exit(1)
+
     try:
-        n = int(argv[1])
-    except BaseException:
-        print('N must be a number')
-        exit(1)
-    if n < 4:
-        print('N must be at least 4')
+        N = int(argv[1])
+    except ValueError:
+        print("N must be a number")
         exit(1)
 
-    solution = []
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
 
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
-
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
-        return True
-
-    solve_queens(0, n, solution)
+    nQueens(N)
